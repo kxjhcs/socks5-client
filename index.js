6684c48ac5b6c423b5d9277dc1d8caa317d6b51b
kxjhcs <kxjhcs@gmail.com>
2016-07-04 21:20:10 +0800

    Update index.js
    support upload username:pwd

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ var Socket = require('./lib/Socket');
 exports.Socket = Socket;
 
 exports.createConnection = function(options) {
-	var socksSocket, socksHost, socksPort;
+	var socksSocket, socksHost, socksPort, socksUserName, socksPwd;
 
 	socksHost = options.socksHost || 'localhost';
 	socksPort = options.socksPort || 1080;
+	socksUserName = options.socksUserName;
+	socksPwd = options.socksPwd;
 
-	socksSocket = new Socket(socksHost, socksPort);
+	socksSocket = new Socket(socksHost, socksPort, socksUserName, socksPwd);
 
 	return socksSocket.connect(options.port, options.host);
 };
